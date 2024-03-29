121502a29b622895f481a07f7efdb3ece46daa3a
Kim Oswalt <130675196+keoswalt@users.noreply.github.com>
2024-03-29 10:12:56 -0500

    Try removing slash from image path on artists

--- data.js
+++ data.js
@@ -45,9 +45,9 @@ class Painting extends Art {
 
 // Adding Artists
 
-const picasso = new Artist("Pablo Picasso", "/images/artists/Picasso.png", "picasso");
-const georgia = new Artist("Georgia O'Keefe", "/images/artists/Georgia.png", "georgia");
-const matisse = new Artist("Henri Matisse", "/images/artists/Matisse.png", "matisse");
+const picasso = new Artist("Pablo Picasso", "images/artists/Picasso.png", "picasso");
+const georgia = new Artist("Georgia O'Keefe", "images/artists/Georgia.png", "georgia");
+const matisse = new Artist("Henri Matisse", "images/artists/Matisse.png", "matisse");
 
 // Adding Art
 
